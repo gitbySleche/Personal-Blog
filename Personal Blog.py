d70ed91d0fd04915a7589f2f890a9271e66abe91
Miguel <miguel.goncalves.p.leite@gmail.com>
2026-06-08 13:34:53 +0100

    Added edit/delete functionality.

--- Personal Blog.py
+++ Personal Blog.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -25,18 +25,38 @@ def admin():
 
         for id in data['Articles']:
             
-            article_titles.append(id, data['Articles'][id]['Article Title'])
+            article_titles.append((id, data['Articles'][id]['Article Title']))
 
         return render_template('admin.html', article_titles=article_titles)
     
-    else:
-        return render_template('admin.html')
+    elif request.method == 'POST':
+        
+        confirmation = request.form['confirmation']
+        if confirmation == 'Yes':
+            
+            id = request.form['article_id']
+
+            with open('Articles.json', 'r') as file:
+                data = json.load(file)
+
+            data['Articles'].pop(id)
+
+            with open('Articles.json', 'w') as file:
+                json.dump(data, file, indent=4)
+
+            return redirect(url_for('admin'))
+        
+        elif confirmation == 'No':
+            
+            return redirect(url_for('admin'))
+        
+    return render_template('admin.html')
 
 @app.route('/admin/new', methods=['GET', 'POST'])
 def new():
 
     if request.method =='GET':
-        return render_template('article_form.html')
+        return render_template('new_article.html')
     
     else:
         if os.path.exists('Articles.json'):
@@ -58,16 +78,59 @@ def new():
     date = request.form['date']
     text = request.form['text']
     data['Articles'][new_id] = {'Article Title':title, 'Date of publishing':date, 'Content':text}
+    new_article = data['Articles'][new_id]['Article Title']
 
     with open('Articles.json', 'w') as file:
         json.dump(data, file, indent=4)
+    
+    return render_template('new_article.html', Publish=new_article)
 
-    return render_template('article_form.html')
-
-@app.route('/admin/edit', methods=['GET', 'POST'])
+@app.route('/admin/edit', methods=['POST'])
 def edit():
     
+    with open('Articles.json', 'r') as file:
+        data = json.load(file)
 
+    id = request.form['article_id']
+    title = data['Articles'][id]['Article Title']
+    date = data['Articles'][id]['Date of publishing']
+    content = data['Articles'][id]['Content']
+    
+    return render_template('edit_article.html', title=title, date=date, content=content, id=id)
+
+@app.route('/admin/update', methods=['POST'])
+def update():
+
+    id = request.form['article_id']
+    title = request.form['title']
+    date = request.form['date']
+    text = request.form['text']
+
+    with open('Articles.json', 'r') as file:
+        data = json.load(file)
+    
+    data['Articles'][id]['Article Title'] = title
+    data['Articles'][id]['Date of publishing'] = date
+    data['Articles'][id]['Content'] = text
+
+    with open('Articles.json', 'w') as file:
+        json.dump(data, file, indent=4)
+    
+    return render_template('edit_article.html', title=title, date=date, content=text, changes_saved=title)
+
+@app.route('/admin/delete_confirmation', methods=['POST'])
+def delete_confirmation():
+
+    id = request.form['article_id']
+    
+    with open('Articles.json', 'r') as file:
+        data = json.load(file)
+    
+    article_titles = []
+    for article_id in data['Articles']:
+        article_titles.append((article_id, data['Articles'][article_id]['Article Title']))
+    
+    return render_template('admin.html', article_titles=article_titles, confirmation=True, delete_id=id)
 
 
 if __name__ == '__main__':
